e03a518fd208cba15ee857afd8b40620134fd0fc
Bhuvanesh <iambhuvanesh@gmail.com>
2023-01-17 14:13:46 +0530

    Bootstrap styling implemented

--- src/my_app.py
+++ src/my_app.py
@@ -2,9 +2,10 @@ import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+import dash_bootstrap_components as dbc
 
-
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}])
 server = app.server
 # -- Import and clean data (importing csv into pandas)
 # df = pd.read_csv("intro_bees.csv")
@@ -25,28 +26,60 @@ app.layout = html.Div([
 
     html.H1("FEATURE ANALYTICS DASHBOARD", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_year",
+    # dcc.Dropdown(id="slct_year",
+    #              options = f_columns,
+    #              multi=False,
+    #              value='PTT_PPG_DIASTOLIC',
+    #              style={'width': "40%", 'color': 'hotpink', 'font-size': 12},
+    #              clearable=False
+    #              ),
+
+    # html.Div(id='output_container', children=[]),
+    # html.Br(),
+    html.Br(),
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H6('SELECT FEATURE :', className='navbar-text navbar-light bg-light text-right'), width=2),
+            dbc.Col([dcc.Dropdown(id="slct_year",
                  options = f_columns,
                  multi=False,
                  value='PTT_PPG_DIASTOLIC',
-                 style={'width': "40%", 'color': 'hotpink', 'font-size': 12},
+                 style={'width': "50%",  'font-size': 14},
                  clearable=False
                  ),
+                #  html.Div(id='output_container', children=[], className='navbar-text'),
+                 html.Br()])
+        ], className="g-1"),
+        dbc.Row([
+            dbc.Col(html.Div(id='plot1', children=[
+             dcc.Graph(id='sd_plot', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+         ])),
+            dbc.Col(html.Div(id='plot1', children=[
+             dcc.Graph(id='df_plot', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+         ]))
+        ]),
 
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-    
-    html.Div(id='plots', children=[
-        html.Div(id='plot1', children=[
-            dcc.Graph(id='sd_plot', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
-        ]),
-        html.Div(id='plot2', children=[
-            dcc.Graph(id='scatter_sys', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
-        ]),
-        html.Div(id='plot3', children=[
-            dcc.Graph(id='scatter_dia', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+        dbc.Row([
+            dbc.Col(html.Div(id='plot2', children=[
+             dcc.Graph(id='scatter_sys', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+         ])),
+            dbc.Col(html.Div(id='plot3', children=[
+             dcc.Graph(id='scatter_dia', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+         ]))
         ])
-    ], style={'display': 'flex','flex-wrap': 'wrap', 'justify-content': 'center',  'width': '80vw', 'height': '100vh'})
+    ]),
+
+    # html.Div(id='plots', children=[
+    #     html.Div(id='plot1', children=[
+    #         dcc.Graph(id='sd_plot', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+    #     ]),
+    #     html.Div(id='plot2', children=[
+    #         dcc.Graph(id='scatter_sys', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+    #     ]),
+    #     html.Div(id='plot3', children=[
+    #         dcc.Graph(id='scatter_dia', figure={}, style={'display': 'flex'}, config={'modeBarButtonsToRemove': ['select2d', 'lasso2d'], 'displaylogo': False})
+    #     ])
+    # ], style={'display': 'flex','flex-wrap': 'wrap', 'justify-content': 'center',  'width': '80vw', 'height': '100vh'})
     
 ])
 
@@ -54,8 +87,9 @@ app.layout = html.Div([
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 @app.callback(
-    [Output(component_id='output_container', component_property='children'),
+    [#Output(component_id='output_container', component_property='children'),
      Output(component_id='sd_plot', component_property='figure'),
+     Output(component_id='df_plot', component_property='figure'),
      Output(component_id='scatter_sys', component_property='figure'),
      Output(component_id='scatter_dia', component_property='figure')],
     [Input(component_id='slct_year', component_property='value')]
@@ -74,11 +108,41 @@ def update_graph(option_slctd):
     # Plotly Express
     fig1 = px.histogram(sd_bp_df, x=option_slctd, nbins=50, color_discrete_sequence = ['hotpink'])
     fig1.update_traces(marker_line_width=1,marker_line_color="white")
+    fig1.update_layout(
+    title={
+        'text': "Distribution of Standard Deviation",
+        'y':0.93,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+    fig1_1 = px.histogram(bp_df, x=option_slctd, nbins=50, color_discrete_sequence = ['hotpink'])
+    fig1_1.update_layout(
+    title={
+        'text': "Distribution of Feature Values",
+        'y':0.93,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+    fig1_1.update_traces(marker_line_width=1,marker_line_color="white")
     fig2 = px.scatter(bp_df, x=option_slctd, y='DBP', color_discrete_sequence = ['hotpink'])
+    fig2.update_layout(
+    title={
+        'text': "DBP vs Selected Feature",
+        'y':0.93,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
     fig3 = px.scatter(bp_df, x=option_slctd, y='SBP', color_discrete_sequence = ['hotpink'])
+    fig3.update_layout(
+    title={
+        'text': "SBP vs Selected Feature",
+        'y':0.93,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
    
 
-    return container, fig1, fig2, fig3
+    return fig1,fig1_1, fig2, fig3
 
 
 # ------------------------------------------------------------------------------
